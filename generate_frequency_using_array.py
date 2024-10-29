8fb19bde2c095aba879ea3d50d71b104d88bc0cf
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-29 19:30:53 +0800

    Started with the pseudocode

---
+++ generate_frequency_using_array.py
@@ -0,0 +1,11 @@
+# ask user to input a number from 1-50 only
+    # insert while loop to input a number again
+    # if input is invalid - number exceeds 50 or user inserted a letter
+        # break loop
+# use list to store user input in each range of numbers
+    # 1-10
+    # 11-20
+    # 21-30
+    # 31-40
+    # 41-50
+# once loop is broken, display a tally of frequency for each range
