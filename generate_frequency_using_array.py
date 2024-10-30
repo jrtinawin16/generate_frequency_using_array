dce78ff7d274008957e4bb2a8ec6b20e3a6686a1
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-31 02:00:35 +0800

    Fixing if statements in loop

--- generate_frequency_using_array.py
+++ generate_frequency_using_array.py
@@ -13,11 +13,11 @@
 # define dictionary for random numbers
 data_inserted_numbers = {}
 data_frequency = {
-        "1-10" : 0,
-        "11-20" : 0,
-        "21-30" : 0,
-        "31-40" : 0,
-        "41-50" : 0
+    "1-10" : 0,
+    "11-20" : 0,
+    "21-30" : 0,
+    "31-40" : 0,
+    "41-50" : 0
 }
 # loop 1 - ask user to input a number from 1-50
 while True:
@@ -25,26 +25,22 @@ while True:
         random_number = int(input("Please enter a number from 1-50: "))
         if random_number >= 1 and random_number <= 50:
             if random_number == 1 and random_number <= 10:
-                data_frequency ["1-10"] += 1
+                data_frequency["1-10"] += 1
             elif random_number == 11 and random_number <= 20:
-                data_frequency ["11-20"] += 1
+                data_frequency["11-20"] += 1
             elif random_number == 21 and random_number <= 30:
-                data_frequency ["21-30"] += 1
+                data_frequency["21-30"] += 1
             elif random_number == 31 and random_number <= 40:
-                data_frequency ["31-40"] += 1
+                data_frequency["31-40"] += 1
             elif random_number == 41 and random_number <= 50:
-                data_frequency ["41-50"] += 1
+                data_frequency["41-50"] += 1
         elif random_number == 0:
             break
         elif random_number > 50:
             break
     except:
-        break    
+        break     
 
-    
-
-for entry in data_inserted_numbers.items():
-  
-
-print("The frequency of inserted numbers in each range are as follows:")
-print(entry)
+print("The frequency of inserted numbers in each range are:")
+for range, count in data_frequency.items():
+    print(f"{range}:  {count}")
